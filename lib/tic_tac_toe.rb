b0adfc71d256cc9d7fef85dec10248ac8685463e
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-11 21:21:31 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,30 @@
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0, 1, 2], #top row
+    [3, 4, 5], #middle row
+    [6, 7, 8], #bottom row
+    [0, 3, 6], #first column
+    [1, 4, 7], #second column
+    [2, 5, 8], #third column
+    [0, 4, 8], #top left to bottom right diagonal
+    [2, 4, 6] #top right to bottom left diagonal
+  ]
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  def display_row(row)
+    puts " #{row[0]} | #{row[1]} | #{row[2]} "
+  end
+  def display_line
+    puts "----------"
+  end
+  def display_board
+    display_row(@board[0..2])
+    display_line
+    display_row(@board[3..5])
+    display_line
+    display_row(@board[6..8])
+  end
+
+
+end
